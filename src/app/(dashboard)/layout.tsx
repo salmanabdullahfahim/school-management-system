c37784386a14e8d2d3c8e41177b4f184047710ac
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-09-22 12:45:03 +0600

    added sidebar Menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,15 +1,30 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const dashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen flex">
       {/* left */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        L
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
+        {/* Logo */}
+        <Link
+          href="/"
+          className="flex justify-center items-center lg:justify-start gap-2"
+        >
+          <Image src={"/logo.png"} alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">FSchool</span>
+        </Link>
+
+        {/* Menu */}
+        <Menu />
       </div>
       {/* right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
-        R
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
       </div>
     </div>
   );
