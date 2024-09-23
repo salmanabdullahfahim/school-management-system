50c5cced809d18c748a20c745d83ad40ef1f59d1
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-09-23 20:52:47 +0600

    Custom scroller added

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,22 +7,19 @@ import React from "react";
 const dashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen flex">
-      {/* left */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
-        {/* Logo */}
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 overflow-y-scroll">
         <Link
           href="/"
-          className="flex justify-center items-center lg:justify-start gap-2"
+          className="flex items-center justify-center lg:justify-start gap-2"
         >
-          <Image src={"/logo.png"} alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">FSchool</span>
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block font-bold">SchooL</span>
         </Link>
-
-        {/* Menu */}
         <Menu />
       </div>
-      {/* right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
