108e4b7ccc9f5f63ba97620943ac86b1087d46a2
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-09-23 21:47:01 +0600

    Students count chart added

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
 
@@ -12,6 +13,14 @@ const AdminPage = () => {
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
+        </div>
+
+        {/* Charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
         </div>
       </div>
 
