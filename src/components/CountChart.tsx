1d3f7962185c1f602e6ed6b642218355e1fbbbdb
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-09-24 21:32:41 +0600

    fix count chart

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -64,7 +64,7 @@ const CountChart = () => {
         </div>
         <div className="flex flex-col gap-1">
           <div className="w-5 h-5 bg-lamaYellow rounded-full" />
-          <h1 className="font-bold">1,234</h1>
+          <h1 className="font-bold">2,433</h1>
           <h2 className="text-xs text-gray-300">Girls (45%)</h2>
         </div>
       </div>
